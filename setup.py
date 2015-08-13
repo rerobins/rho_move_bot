67ffbe02e0e69cc97b55f35a45cf191dcb31539e
Robert Robinson <rerobins@gmail.com>
2015-08-13 13:02:00 -0600

    Initial checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='movebot',
+    version='1.0.0',
+    packages=['movebot'],
+    url='',
+    license='BSD',
+    author='Robert Robinson',
+    author_email='rerobins@meerkatlabs.org',
+    description='Move Apps Management bot for Rhobot infrastructure'
+)
