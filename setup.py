2597f7c2e3896ab70f0bad18c8150c7e025f1435
Robert Robinson <rerobins@gmail.com>
2015-09-16 16:07:34 -0600

    Add setup.py and update requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='movebot',
+    name='move_bot',
     version='1.0.0',
-    packages=['movebot'],
+    packages=['move_bot',
+              'move_bot.components',
+              'move_bot.components.commands',
+              'move_bot.components.events',
+              'move_bot.components.update_service',
+              ],
     url='',
     license='BSD',
     author='Robert Robinson',
     author_email='rerobins@meerkatlabs.org',
-    description='Move Apps Management bot for Rhobot infrastructure'
+    description='Moves App connection bot for the Rho infrastructure',
+    install_requires=['rhobot==1.0.0', 'moves==0.1', 'requests==2.7.0', ]
 )
